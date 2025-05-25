7449cef78af6ed1ffdaff18e8626deab4639530e
Nenad Cetic <chedadsp@gmail.com>
2025-05-25 07:05:42 +0200

    some random old changes

--- esp-scoreboard-button/esp-scoreboard-button.ino
+++ esp-scoreboard-button/esp-scoreboard-button.ino
@@ -19,7 +19,7 @@ ESP8266WiFiMulti WiFiMulti;
 
 ButtonDebounce button(D3, 250); // Flash button
 
-int doHTTP(char* option) {
+int doHTTP(const char* option) {  // <-- changed from char* to const char*
 
   if ((WiFiMulti.run() == WL_CONNECTED)) {
     WiFiClient client;
@@ -32,19 +32,10 @@ int doHTTP(char* option) {
     if (http.begin(client, url)) {  // HTTP
 
       Serial.print("[HTTP] GET...\n");
-      // start connection and send HTTP header
       int httpCode = http.GET();
 
-      // httpCode will be negative on error
       if (httpCode > 0) {
-        // HTTP header has been send and Server response header has been handled
         Serial.printf("[HTTP] GET... code: %d\n", httpCode);
-
-        // file found at server
-        //if (httpCode == HTTP_CODE_OK || httpCode == HTTP_CODE_MOVED_PERMANENTLY) {
-          //String payload = http.getString();
-          //Serial.println(payload);
-        //}
       } else {
         Serial.printf("[HTTP] GET... failed, error: %s\n", http.errorToString(httpCode).c_str());
       }
@@ -72,7 +63,7 @@ void buttonCallback(int state) {
 
 void setup() {
 
-  Serial.begin(115200);
+  Serial.begin(38400);
 
   button.setCallback(buttonCallback);
 
